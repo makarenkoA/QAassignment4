d27485e2e214dfc114e82197c8b38bcccec0228c
makarenkoA <Amakarenko1466@conestogac.on.ca>
2019-04-07 12:36:53 -0400

    Created project

---
+++ QAassignment4_Test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QAassignment4_Test
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
